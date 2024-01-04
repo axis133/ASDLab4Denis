84ff9d0c0580eaa16b01278255e6021f0e09f5ff
axis133 <148345418+axis133@users.noreply.github.com>
2024-01-04 19:44:19 +0300

    Добавьте файлы проекта.

---
+++ ASDLab4Denis/ASDLab4Denis.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int V = 4;
+int E = 4;
+
+void convertToIncidenceMatrix(vector<vector<int>> adjMatrix) {
+    vector<vector<int>> incidenceMatrix(V, vector<int>(E));
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (adjMatrix[i][j] == 1) {
+                int edgeNumber = 0;
+                for (int k = 0; k < V; k++) {
+                    if (k != i && adjMatrix[k][j] == 1) {
+                        edgeNumber = k;
+                        break;
+                    }
+                }
+
+                incidenceMatrix[i][edgeNumber] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < E; j++) {
+            cout << incidenceMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<vector<int>> adjMatrix = { {0, 1, 1, 1},
+                                     {1, 0, 1, 0},
+                                     {1, 1, 0, 1},
+                                     {1, 0, 1, 0} };
+
+    convertToIncidenceMatrix(adjMatrix);
+
+    return 0;
+}
